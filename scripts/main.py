c2103eef2fc06af4043d9ba5f24ae3aaadf4b2dd
AIV Devnet Sandbox <angel.inglese@gmail.com>
2020-11-16 00:01:22 -0500

    first good attempt

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 from genie.testbed import load
 
+identifier = 0
 tb = load('yaml/my_testbed.yaml')
 
 """This module search for info inside testbed hosts."""
@@ -7,14 +8,16 @@ text_file = open("testing.xlsx", "w")
 ### first line
 n = text_file.write('Id,Device Name, Serial\n')
 
+
 for device_name in tb.devices:
+    identifier = identifier + 1
     dev = tb.devices[device_name]
     dev.connect(log_stdout=False)
     p1 = dev.parse('show inventory')
-###    print(p1['main']['chassis']['IOSv']['sn'])
-    print(device_name + " has a serial number: " + p1['main']['chassis']['IOSv']['sn'])
-### next lines
-    n = text_file.write('1,' + device_name +', ' + p1['main']['chassis']['IOSv']['sn'] + '\n')
+    ###    print(p1['main']['chassis']['IOSv']['sn'])
+    print(str(identifier)+'. ' + device_name + " has a serial number: " + p1['main']['chassis']['IOSv']['sn'])
+    ### next lines
+    n = text_file.write(str(identifier) +', ' + device_name +', ' + p1['main']['chassis']['IOSv']['sn'] + '\n')
 
 
 text_file.close()
