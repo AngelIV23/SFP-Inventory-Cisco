dd551778c11f6e43803716d527dc8cc5071a4c2d
AIV Devnet Sandbox <angel.inglese@gmail.com>
2020-11-15 22:06:19 -0500

    updates

---
+++ scripts/main.py
@@ -0,0 +1,13 @@
+from genie.testbed import load
+
+tb = load('yaml/my_testbed.yaml')
+
+
+for device_name in tb.devices:
+    print(device_name)
+    dev = tb.devices[device_name]
+    dev.connect(log_stdout=False)
+    p1 = dev.parse('show inventory')
+    print(p1['main']['chassis']['IOSv']['sn'])
+
+    print(device_name + " has a serial number: " + p1['main']['chassis']['IOSv']['sn'])
